759039f1cebb93b3fb62029171c16a381e96c0aa
tranlam-2002 <tranlam2782002@gamil.com>
2024-03-13 17:29:38 +0700

    hh

--- src/layout/components/Chart/PieChart/index.js
+++ src/layout/components/Chart/PieChart/index.js
@@ -1,62 +1,73 @@
 import React from "react";
-import { PieChart, Pie, Tooltip, Cell, Legend, ResponsiveContainer } from "recharts";
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  Cell,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
+const PieChartLayout = ({ data }) => {
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"]; // Mảng các màu sắc cho các khoản thu
 
-const PieChartLayout = ({ data}) =>  {
-  
-    const renderCustomizedLabel = ({
-        cx,
-        cy,
-        midAngle,
-        innerRadius,
-        outerRadius,
-        value,
-        index,
-        color
-      }) => {
-        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-        const x = cx + radius * Math.cos(-midAngle * (Math.PI / 180));
-        const y = cy + radius * Math.sin(-midAngle * (Math.PI / 180));
-        let total = 0;
-         data.map(i => {
-          total += i.value
-        })
-        const percentage = `${((value/total)*100).toFixed(2)}%`;
-       
-        return (
-          <text x={x} y={y} fill={color} textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-            {(percentage)}
-          </text>
-        );
-      };
-      
-      const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']; // Mảng các màu sắc cho các khoản thu
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    value,
+    index,
+    color,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * (Math.PI / 180));
+    const y = cy + radius * Math.sin(-midAngle * (Math.PI / 180));
+    let total = 0;
+    data.map((i) => {
+      return (total += i.value);
+    });
+    const percentage = `${((value / total) * 100).toFixed(2)}%`;
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill={color}
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {percentage}
+      </text>
+    );
+  };
   return (
-        <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          {/* Biểu đồ thu */}
-          <Pie
-            data={data}
-            nameKey="name"
-            cx="20%"
-            cy="40%"
-            innerRadius={60}
-            outerRadius={80}
-            fill="#8884d8"
-            paddingAngle={5}
-            dataKey="value"
-            //dataKey="value"
-            //outerRadius={100} 
-            //fill={COLORS[['#FF5733', '#82ca9d', '#8884d8']]} // Sử dụng màu từ mảng COLORS
-            label={renderCustomizedLabel}
-          />
-          {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          <Tooltip />
-          <Legend align="center" verticalAlign="middle" layout="vertical" />
-        </PieChart>
-        </ResponsiveContainer>
+    <ResponsiveContainer width="100%" height={300}>
+      <PieChart>
+        {/* Biểu đồ thu */}
+        <Pie
+          data={data}
+          nameKey="name"
+          cx="20%"
+          cy="40%"
+          innerRadius={60}
+          outerRadius={80}
+          fill="#8884d8"
+          paddingAngle={5}
+          dataKey="value"
+          //dataKey="value"
+          //outerRadius={100}
+          //fill={COLORS[['#FF5733', '#82ca9d', '#8884d8']]} // Sử dụng màu từ mảng COLORS
+          label={renderCustomizedLabel}
+        />
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+        <Tooltip />
+        <Legend align="center" verticalAlign="middle" layout="vertical" />
+      </PieChart>
+    </ResponsiveContainer>
   );
 };
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
-import styles from "./Home.scss";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from "recharts";
+
 import PieChartLayout from "~/layout/components/Chart/PieChart";
 import BarChartLayout from "~/layout/components/Chart/BarChart";
-
-
 
 const Home = () => {
   // Khai báo các biến và khởi tạo giá trị ban đầu
@@ -12,73 +9,72 @@ const Home = () => {
   const [tongTienChi, setTongTienChi] = useState(1000000000); // Tổng tiền chi
   const [tongTienThu, setTongTienThu] = useState(1000000000); // Tổng tiền thu
   const data = [
-    { name: 'Thứ 2', thu: 500000, chi: 300000 },
-    { name: 'Thứ 3', thu: 700000, chi: 400000 },
-    { name: 'Thứ 4', thu: 600000, chi: 350000 },
-    { name: 'Thứ 5', thu: 800000, chi: 450000 },
-    { name: 'Thứ 6', thu: 900000, chi: 500000 },
-    { name: 'Thứ 7', thu: 1000000, chi: 600000 },
-    { name: 'Chủ nhật', thu: 1200000, chi: 700000 }
+    { name: "Thứ 2", thu: 500000, chi: 300000 },
+    { name: "Thứ 3", thu: 700000, chi: 400000 },
+    { name: "Thứ 4", thu: 600000, chi: 350000 },
+    { name: "Thứ 5", thu: 800000, chi: 450000 },
+    { name: "Thứ 6", thu: 900000, chi: 500000 },
+    { name: "Thứ 7", thu: 1000000, chi: 600000 },
+    { name: "Chủ nhật", thu: 1200000, chi: 700000 },
   ];
   const dataThu = [
-    { name: 'Lương', value: 5000000 },
-    { name: 'Bán hàng', value: 2000000 },
-    { name: 'Lãi đầu tư', value: 1000000 },
-    { name: 'Lãi', value: 1000000 }
-
+    { name: "Lương", value: 5000000 },
+    { name: "Bán hàng", value: 2000000 },
+    { name: "Lãi đầu tư", value: 1000000 },
+    { name: "Lãi", value: 1000000 },
   ];
- 
+
   const dataChi = [
-    { name: 'Mua sắm', value: 1500000},
-    { name: 'Hóa đơn', value: 800000},
-    { name: 'Tiền ăn', value: 500000}
- 
+    { name: "Mua sắm", value: 1500000 },
+    { name: "Hóa đơn", value: 800000 },
+    { name: "Tiền ăn", value: 500000 },
   ];
   return (
     <div className="home">
-    <div className="bank-card-container">
-      <div className="bank-card">
-        <div className="card-details">
-          <h2>Tổng Tiền Ví S</h2>
-          <p className="card-number">{tongTienViS.toLocaleString()} VND</p>
+      <div className="bank-card-container">
+        <div className="bank-card">
+          <div className="card-details">
+            <h2>Tổng Tiền Ví S</h2>
+            <p className="card-number">{tongTienViS.toLocaleString()} VND</p>
+          </div>
         </div>
+
+        <div className="bank-card">
+          <div className="card-details">
+            <h2>Tổng Tiền Chi</h2>
+            <p className="card-number" style={{ color: "#FF5733" }}>
+              {tongTienChi.toLocaleString()} VND
+            </p>
+          </div>
+        </div>
+
+        <div className="bank-card">
+          <div className="card-details">
+            <h2>Tổng Tiền Thu</h2>
+            <p className="card-number" style={{ color: "#8884d8" }}>
+              {tongTienThu.toLocaleString()} VND
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="chart-container">
+        {/* Biểu đồ cột */}
+        <h2 style={{ textAlign: "center" }}>Biểu đồ cột</h2>
+        <BarChartLayout data={data} />
       </div>
 
-      <div className="bank-card">
-        <div className="card-details">
-          <h2>Tổng Tiền Chi</h2>
-          <p className="card-number" style={{ color: "#FF5733"}}>{tongTienChi.toLocaleString()} VND</p>
-        </div>
-      </div>
-
-      <div className="bank-card">
-        <div className="card-details">
-          <h2>Tổng Tiền Thu</h2>
-          <p className="card-number" style={{ color: "#8884d8" }}>{tongTienThu.toLocaleString()} VND</p>
-        </div>
-      </div>
-     </div>
-     <div className="chart-container">
-      {/* Biểu đồ cột */}
-        <h2 style={{textAlign: "center"}}>Biểu đồ cột</h2>
-          <BarChartLayout data={data}/>
-     </div>
-       
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h2>Biểu đồ thu tháng</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h2>Biểu đồ thu tháng</h2>
             <PieChartLayout data={dataThu} />
-        </div>
-        <div className="col">
-        <h2>Biểu đồ Chi tháng</h2>
-          <PieChartLayout  data={dataChi}/>
+          </div>
+          <div className="col">
+            <h2>Biểu đồ Chi tháng</h2>
+            <PieChartLayout data={dataChi} />
+          </div>
         </div>
       </div>
-    </div>
-     
-      
-      
     </div>
   );
 };
